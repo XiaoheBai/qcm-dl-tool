5e56f2e32618563725a40308204ab63f0f45174a
柏小河 <baixiaohe@baixiaohedeMacBook-Pro.local>
2020-04-26 19:00:35 +0800

    modified the lncosh activation function in order to avoid overflow

--- deepnet.py
+++ deepnet.py
@@ -20,8 +20,12 @@ def initialize_parameters(L, n_h, seed, sigma):
     np.random.seed(seed)
 
     parameters = {}
-    parameters['W1'] = sigma * np.random.randn(n_h, L)
-    parameters['b1'] = np.zeros((n_h, 1))
+    parameters['W1'] = np.zeros((n_h, L), dtype=complex)
+    for i in range(n_h):
+        for j in range(L):
+            parameters['W1'].itemset((i,j), random.gauss(0,sigma) + random.gauss(0,sigma) * 1j)
+
+    parameters['b1'] = np.zeros((n_h, 1), dtype=complex)
 
     
     assert(parameters['W1'].shape == (n_h, L))
@@ -53,6 +57,32 @@ def relu(Z):
 
     assert(A.shape == Z.shape)
     
+
+    return A, Z
+
+# CReLU
+
+def crelu(Z):
+    """ 
+    Implement the ReLU function. Reference: deep complex net.
+
+    Arguments:
+    Z -- Complex Output of the linear layer, of any shape
+
+    Returns:
+    A -- Post-activation parameter, of the same shape as Z
+    Z -- stored for computing the backward pass efficiently
+    """
+
+    A = np.copy(Z)
+    length = len(Z)
+    width = len(Z[0])
+
+    for i in range(length):
+        for j in range(width):
+            A.itemset((i,j), max(0, Z[i,j].real + max(0, Z[i,j].imag * 1j)))
+
+    assert(A.shape == Z.shape)
 
     return A, Z
 
@@ -118,26 +148,41 @@ def tanh(Z):
     
     return A, Z
 
-# Log(Cosh)
+# Log(Cosh) modified according to the Carleo netket
 
-def logcosh(Z):
+def real_lncosh_modified(Z):
     """
-    Implements the log(cosh) activation in numpy
-    
+    Implement the MODIFIED lncosh activation real np matrix, in order to avoid the "inf problem".
+    Reference: Carleo netket
+
     Arguments:
-    Z -- numpy array of any shape
+    Z -- REAL numpy array of any shape
     
     Returns:
-    A -- output of tranh(z), same shape as Z
+    real_lncosh_modified(Z) -- np matrix with the same shape as Z
+    """
+
+    # return np.where( np.abs(Z) <= 12. , real_lncosh(Z), np.abs(Z) - np.log(2.) )
+    return np.where( np.abs(Z) <= 12. , np.log(np.cosh(Z)), np.abs(Z) - np.log(2) )
+
+
+def complex_lncosh(Z):
+    """
+    Implements the COMPLEX ln(cosh) activation in numpy.
+    Reference: Carleo netket
+    
+    Arguments:
+    Z -- COMPLEX numpy array of any shape
+    
+    Returns:
+    A -- output of lncosh(z), same shape as Z
     Z -- returns Z as well, useful during backpropagation
     """
     
-    A = np.log( np.cosh(Z) )
-    
-
-    assert(A.shape == Z.shape)
+    A = real_lncosh_modified(np.real(Z)) + np.log( np.cos(np.imag(Z)) + 1j * np.tanh(np.real(Z)) * np.sin(np.imag(Z)))
     
     return A, Z
+
 
 # Feedforward
 
@@ -177,10 +222,10 @@ def linear_activation_forward(A_prev, W, b, activation):
         Z = np.dot(W, A_prev) + b
         A, cache = leaky_relu(Z)
 
-    elif activation == "logcosh":
+    elif activation == "lncosh":
         # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
         Z = np.dot(W, A_prev) + b
-        A, cache = logcosh(Z)
+        A, cache = complex_lncosh(Z)
 
     
     assert (A.shape == (W.shape[0], A_prev.shape[1]))
@@ -209,7 +254,7 @@ def FFNN_paper_model(X, parameters):
     b1 = parameters["b1"]
     
     # Feedforward
-    A, Z = linear_activation_forward(X, W1, b1, 'logcosh')
+    A, Z = linear_activation_forward(X, W1, b1, 'lncosh')
     Y = np.sum(A, keepdims=True)
     phi = np.exp(Y)
     phi = np.squeeze(phi)
@@ -222,13 +267,19 @@ def FFNN_paper_model(X, parameters):
 # L = 6
 
 # # input
-# X = np.array([-1, 1, -1, 1, -1, 1]).reshape((L,1))
+# X = np.array([-1, 1, 1, -1, -1, 1]).reshape((L,1))
 
 # # Initialize parameters, then retrieve W1, b1, W2, b2. 
 # parameters = initialize_parameters(L, n_h = 2*L, seed=1234, sigma=0.01)
-# print(parameters)
+# # print(parameters)
 
 
 # # Feedforward
-# phi, Y, Z = FFNN_paper_model(X, parameters)
-# print(phi, Y)
+# A, Z = linear_activation_forward(X, parameters['W1'], parameters['b1'], 'logcosh')
+# print(Z)
+# print(A)
+# Y= np.sum(A, keepdims=True)
+# print(Y)
+# # print(phi)
+
+
