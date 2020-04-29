change cmpute_S function according to the correct formula. For L = 6, the energy can already drop to -9. However, there are two problems: 1. it cannot be stable at -9. 2. it can drop to -10, which is lower than the ground state energy.

cost_function.py
@@ -97,7 +97,8 @@ def markov_chain(X, Y, parameters, n_sample = 1000):
         propose_Y = deepnet.FFNN_paper_model(propose_X.T, parameters)[0]
 
         """ Accept / Reject """
-        r = min(1, np.exp(2 * (propose_Y.real - Y.real)))
+        ratio = lambda propose_Y, Y: np.exp(2) if (propose_Y.real - Y.real)>=1. else np.exp(2 * (propose_Y.real - Y.real))
+        r = min(1 , ratio(propose_Y, Y))
         # print(propose_X, propose_Y, train_set_Y[n-1], r)
         random_number = random.uniform(0, 1)
 

grad_check.py
@@ -19,15 +19,13 @@ def dictionary_to_vector(parameters):
     Returns:
     theta -- a big vector containing all parameters with the sequence: 
                     W1[1], W1[2], ..., b1, W2[1], ... b2, ...
-    keys -- Information about the sequence in 'theta'
     """
-    keys = []
+    
     count = 0
     for key in ["W1", "b1"]:
         
         # flatten parameter
         new_vector = np.reshape(parameters[key], (-1,1))
-        keys = keys + [key]*new_vector.shape[0]
         
         if count == 0:
             theta = new_vector
@@ -35,16 +33,17 @@ def dictionary_to_vector(parameters):
             theta = np.concatenate((theta, new_vector), axis=0)
         count = count + 1
     
-    return theta, keys
+    return theta
 
 # Reshape the tiny shifted big vector theta back to the parameters dictionary
-def vector_to_dictionary(theta):
+def vector_to_dictionary(theta, L):
     """
     Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
     
     Arguments: 
     theta -- a big vector containing all parameters with the sequence: 
                     W1[1], W1[2], ..., b1, W2[1], ... b2, ...
+    L -- size of the quantum system
 
     Returns:
     parameters -- python dictionary containing your parameters:
@@ -52,7 +51,7 @@ def vector_to_dictionary(theta):
                     bl -- bias matrix of the visible (0-th) layer, shape (layer_dims[0], 1)
     """
     parameters = {}
-    L = 6
+    
     parameters["W1"] = theta[:2*L**2].reshape((2*L,L))
     parameters["b1"] = theta[2*L**2:].reshape((-1,1))
 
@@ -60,197 +59,208 @@ def vector_to_dictionary(theta):
 
 
 # Reshape gradients of Y (O_grads) to vector
-def O_gradients_to_vector(O_grads):
+def O_gradients_to_vector(O_W1, O_b1):
     """
     Roll all our gradients dictionary into a single vector satisfying our specific required shape.
     
     Arguments:
-    O_grads -- python dictionary containing O Operator applied on parameters
-                    O_Wl -- Gradient of Y with respect to Wl (matrix of Wl.shape)
-                    O_bl -- Gradient of Y with respect to bl (matrix of bl.shape)
+    O_Wl -- Gradient of Y with respect to Wl (matrix of Wl.shape)
+    O_bl -- Gradient of Y with respect to bl (matrix of bl.shape)
 
     Returns:
     O_theta -- a big vector containing all O_grads with the sequence: 
                     O_W1[1], O_W1[2], ..., O_b1, O_W2[1], ... O_b2, ...
     """
     
-    count = 0
-    for key in ["O_W1", "O_b1"]:
-        # flatten parameter
-        new_vector = np.reshape(O_grads[key], (-1,1))
-        
-        if count == 0:
-            O_theta = new_vector
-        else:
-            O_theta = np.concatenate((O_theta, new_vector), axis=0)
-        count = count + 1
+    
+    new_vector = np.reshape(O_W1, (-1,1))
+    O_theta = np.copy(new_vector)
+    
+    new_vector = np.reshape(O_b1, (-1,1))
+    O_theta = np.concatenate((O_theta, new_vector), axis=0)
 
     return O_theta
 
 # Reshape gradients of cost function Hamiltonian (grads) to vector
-def gradients_to_vector(grads):
+def f_to_vector(f):
     """
     Roll all our gradients dictionary into a single vector satisfying our specific required shape.
     
     Arguments:
-    grads -- python dictionary contraining your gradients
-                    delta_Wl -- Gradient of the cost with respect to Wl
-                    delta_bl -- Gradient of the cost with respect to bl
+    f -- python dictionary contraining your gradients
+                    f_Wl -- Gradient of the cost with respect to Wl
+                    f_bl -- Gradient of the cost with respect to bl
     
     Returns:
-    delta_theta -- a big vector containing all gradients with the sequence: 
-                    delta_W1[1], delta_W1[2], ..., delta_b1, delta_W2[1], ... delta_b2, ...
+    f_theta -- a big vector containing all gradients with the sequence: 
+                    f_W1[1], f_W1[2], ..., f_b1, f_W2[1], ... f_b2, ...
     """
     
     count = 0
-    for key in ["delta_W1", "delta_b1"]:
+    for key in ["f_W1", "f_b1"]:
         # flatten parameter
-        new_vector = np.reshape(grads[key], (-1,1))
+        new_vector = np.reshape(f[key], (-1,1))
         
         if count == 0:
-            delta_theta = new_vector
+            f_theta = new_vector
         else:
-            delta_theta = np.concatenate((delta_theta, new_vector), axis=0)
+            f_theta = np.concatenate((f_theta, new_vector), axis=0)
         count = count + 1
 
-    return delta_theta
+    return f_theta
 
 
 
 
 ### Gradient Check
 
-# O gradient check
-def O_gradients_check(parameters, O_grads, X, Y, epsilon = 1e-7):
-    """
-    Checks if backward_propagation computes correctly the gradient of Y by forward_propagation_n
+# # O gradient check
+# def O_gradients_check(parameters, O_grads, X, Y, epsilon = 1e-7):
+#     """
+#     Checks if backward_propagation computes correctly the gradient of Y by forward_propagation_n
     
-    Arguments:
-    parameters -- python dictionary containing your parameters "Wl", "bl"
-    O_grads -- output of linear_activation_backward, contains O gradients of Y with respect to the parameters. 
-    X -- input datapoint, of shape (input size, 1)
-    Y -- output of the deepnet
-    epsilon -- tiny shift to the input to compute approximated gradient with formula:
-                  O_gradapprox = (Y(+) - Y(-))/(2*epsilon)
+#     Arguments:
+#     parameters -- python dictionary containing your parameters "Wl", "bl"
+#     O_grads -- output of linear_activation_backward, contains O gradients of Y with respect to the parameters. 
+#     X -- input datapoint, of shape (input size, 1)
+#     Y -- output of the deepnet
+#     epsilon -- tiny shift to the input to compute approximated gradient with formula:
+#                   O_gradapprox = (Y(+) - Y(-))/(2*epsilon)
     
-    Returns:
-    difference -- difference between the approximated gradient and the backward propagation gradient:
-                  || O_grads - O_gradapprox ||_2 / (|| O_grads ||_2 + || O_gradapprox ||_2)
-    """
+#     Returns:
+#     difference -- difference between the approximated gradient and the backward propagation gradient:
+#                   || O_grads - O_gradapprox ||_2 / (|| O_grads ||_2 + || O_gradapprox ||_2)
+#     """
     
-    # Set-up variables
-    theta, _ = dictionary_to_vector(parameters)
-    O_theta = O_gradients_to_vector(O_grads)
-    num_parameters = theta.shape[0]
-    Y_plus = np.zeros((num_parameters, 1)) # Vector
-    Y_minus = np.zeros((num_parameters, 1))
-    O_gradapprox = np.zeros((num_parameters, 1))
+#     # Set-up variables
+#     theta, _ = dictionary_to_vector(parameters)
+#     O_theta = O_gradients_to_vector(O_grads)
+#     num_parameters = theta.shape[0]
+#     Y_plus = np.zeros((num_parameters, 1)) # Vector
+#     Y_minus = np.zeros((num_parameters, 1))
+#     O_gradapprox = np.zeros((num_parameters, 1))
     
-    # Compute gradapprox
-    for i in range(num_parameters):
+#     # Compute gradapprox
+#     for i in range(num_parameters):
         
-        # Compute Y_plus[i]. Inputs: "theta, epsilon". Output = "Y_plus[i]".
-        thetaplus = np.copy(theta)                   # Step 1
-        thetaplus[i][0] = thetaplus[i][0] + epsilon                   # Step 2
-        Y_plus[i] = deepnet.FFNN_paper_model(X, vector_to_dictionary( thetaplus))[1]     # Step 3
+#         # Compute Y_plus[i]. Inputs: "theta, epsilon". Output = "Y_plus[i]".
+#         thetaplus = np.copy(theta)                   # Step 1
+#         thetaplus[i][0] = thetaplus[i][0] + epsilon                   # Step 2
+#         Y_plus[i] = deepnet.FFNN_paper_model(X, vector_to_dictionary( thetaplus))[1]     # Step 3
         
-        # Compute Y_minus[i]. Inputs: "theta, epsilon". Output = "Y_minus[i]".
-        thetaminus = np.copy(theta)                            # Step 1
-        thetaminus[i][0] = thetaminus[i][0] - epsilon              # Step 2        
-        Y_minus[i] = deepnet.FFNN_paper_model(X, vector_to_dictionary( thetaminus))[1]  # Step 3
+#         # Compute Y_minus[i]. Inputs: "theta, epsilon". Output = "Y_minus[i]".
+#         thetaminus = np.copy(theta)                            # Step 1
+#         thetaminus[i][0] = thetaminus[i][0] - epsilon              # Step 2        
+#         Y_minus[i] = deepnet.FFNN_paper_model(X, vector_to_dictionary( thetaminus))[1]  # Step 3
         
-        # Compute gradapprox[i]
-        O_gradapprox[i] = (Y_plus[i] - Y_minus[i]) / (2*epsilon)
+#         # Compute gradapprox[i]
+#         O_gradapprox[i] = (Y_plus[i] - Y_minus[i]) / (2*epsilon)
     
-    # Compare gradapprox to backward propagation gradients by computing difference.
-    numerator = np.linalg.norm(O_theta - O_gradapprox)           # Step 1'
-    denominator = np.linalg.norm(O_theta) + np.linalg.norm(O_gradapprox)         # Step 2'
-    difference = numerator / denominator         # Step 3'
+#     # Compare gradapprox to backward propagation gradients by computing difference.
+#     numerator = np.linalg.norm(O_theta - O_gradapprox)           # Step 1'
+#     denominator = np.linalg.norm(O_theta) + np.linalg.norm(O_gradapprox)         # Step 2'
+#     difference = numerator / denominator         # Step 3'
 
-    if difference > 1e-7:
-        print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
-    else:
-        print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+#     if difference > 1e-7:
+#         print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+#     else:
+#         print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
     
-    return difference
+#     return difference
  
-# Gradients check
-def gradients_check(parameters, grads, train_set, train_set_phi, epsilon = 1e-7):
-    """
-    Checks if the gradient of Hamiltonian computes correctly
+# # Gradients check
+# def gradients_check(parameters, grads, train_set, train_set_phi, epsilon = 1e-7):
+#     """
+#     Checks if the gradient of Hamiltonian computes correctly
     
-    Arguments:
-    parameters -- python dictionary containing your parameters "Wl", "bl"
-    grads -- output of compute_gradients, contains gradients of Hamiltonian with respect to the parameters. 
-    train_set -- Training set with each row representing one |sigma>
-    train_set_phi -- The wavefunction of each spin configuration in train_set
-    epsilon -- tiny shift to the input to compute approximated gradient with formula:
-                  O_gradapprox = (H(+) - H(-))/(2*epsilon)
+#     Arguments:
+#     parameters -- python dictionary containing your parameters "Wl", "bl"
+#     grads -- output of compute_gradients, contains gradients of Hamiltonian with respect to the parameters. 
+#     train_set -- Training set with each row representing one |sigma>
+#     train_set_phi -- The wavefunction of each spin configuration in train_set
+#     epsilon -- tiny shift to the input to compute approximated gradient with formula:
+#                   O_gradapprox = (H(+) - H(-))/(2*epsilon)
     
-    Returns:
-    difference -- difference between the approximated gradient and the backward propagation gradient:
-                  || grads - gradapprox ||_2 / (|| grads ||_2 + || gradapprox ||_2)
-    """
+#     Returns:
+#     difference -- difference between the approximated gradient and the backward propagation gradient:
+#                   || grads - gradapprox ||_2 / (|| grads ||_2 + || gradapprox ||_2)
+#     """
     
-    # Set-up variables
-    theta, _ = dictionary_to_vector(parameters)
-    delta_theta = gradients_to_vector(grads)
-    num_parameters = theta.shape[0]
-    H_plus = np.zeros((num_parameters, 1)) # Vector
-    H_minus = np.zeros((num_parameters, 1))
-    gradapprox = np.zeros((num_parameters, 1))
+#     # Set-up variables
+#     theta, _ = dictionary_to_vector(parameters)
+#     delta_theta = gradients_to_vector(grads)
+#     num_parameters = theta.shape[0]
+#     H_plus = np.zeros((num_parameters, 1)) # Vector
+#     H_minus = np.zeros((num_parameters, 1))
+#     gradapprox = np.zeros((num_parameters, 1))
     
-    # Compute gradapprox
-    for i in range(num_parameters):
+#     # Compute gradapprox
+#     for i in range(num_parameters):
         
-        # Compute H_plus[i]. Inputs: "theta, epsilon". Output = "H_plus[i]".
-        thetaplus = np.copy(theta)                   # Step 1
-        thetaplus[i][0] = thetaplus[i][0] + epsilon                   # Step 2
-        H_plus[i] = cost_function.compute_hamiltonian(train_set, train_set_phi, vector_to_dictionary( thetaplus))     # Step 3
-        # print('H_plus',i,':',H_plus[i])
+#         # Compute H_plus[i]. Inputs: "theta, epsilon". Output = "H_plus[i]".
+#         thetaplus = np.copy(theta)                   # Step 1
+#         thetaplus[i][0] = thetaplus[i][0] + epsilon                   # Step 2
+#         H_plus[i] = cost_function.compute_hamiltonian(train_set, train_set_phi, vector_to_dictionary( thetaplus))     # Step 3
+#         # print('H_plus',i,':',H_plus[i])
         
-        # Compute H_minus[i]. Inputs: "theta, epsilon". Output = "H_minus[i]".
-        thetaminus = np.copy(theta)                            # Step 1
-        thetaminus[i][0] = thetaminus[i][0] - epsilon              # Step 2        
-        H_minus[i] = cost_function.compute_hamiltonian(train_set, train_set_phi, vector_to_dictionary( thetaminus))  # Step 3
-        # print('H_minus',i,':',H_minus[i])
+#         # Compute H_minus[i]. Inputs: "theta, epsilon". Output = "H_minus[i]".
+#         thetaminus = np.copy(theta)                            # Step 1
+#         thetaminus[i][0] = thetaminus[i][0] - epsilon              # Step 2        
+#         H_minus[i] = cost_function.compute_hamiltonian(train_set, train_set_phi, vector_to_dictionary( thetaminus))  # Step 3
+#         # print('H_minus',i,':',H_minus[i])
 
-        # Compute gradapprox[i]
-        gradapprox[i] = (H_plus[i] - H_minus[i]) / (2*epsilon)
-        # print('gradapprox',i,':', gradapprox[i])
+#         # Compute gradapprox[i]
+#         gradapprox[i] = (H_plus[i] - H_minus[i]) / (2*epsilon)
+#         # print('gradapprox',i,':', gradapprox[i])
     
-    # Compare gradapprox to backward propagation gradients by computing difference.
-    numerator = np.linalg.norm(delta_theta - gradapprox)           # Step 1'
-    denominator = np.linalg.norm(delta_theta) + np.linalg.norm(gradapprox)         # Step 2'
-    difference = numerator / denominator         # Step 3'
+#     # Compare gradapprox to backward propagation gradients by computing difference.
+#     numerator = np.linalg.norm(delta_theta - gradapprox)           # Step 1'
+#     denominator = np.linalg.norm(delta_theta) + np.linalg.norm(gradapprox)         # Step 2'
+#     difference = numerator / denominator         # Step 3'
 
-    if difference > 1e-7:
-        print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
-    else:
-        print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+#     if difference > 1e-7:
+#         print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+#     else:
+#         print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
     
-    return difference
+#     return difference
 
 ### Test
+if __name__ == '__main__':
+    L = 6
+    # input
+    X = np.array([-1, 1, -1, 1, -1, 1]).reshape((L,1))
 
-# L = 6
-# # input
-# X = np.array([-1, 1, -1, 1, -1, 1]).reshape((L,1))
+    # Initialize parameters, then retrieve W1, b1, W2, b2. 
 
-# # Initialize parameters, then retrieve W1, b1, W2, b2. 
+    parameters = deepnet.initialize_parameters(L, n_h = 2*L, seed=1234, sigma=0.01)
+    
+    # theta = dictionary_to_vector(parameters)
 
-# parameters = deepnet.initialize_parameters(L, [2*L])
+    # print('parameters:', parameters['W1'], parameters['b1'])
+    # print(theta.shape)
+    # print(theta)
 
-# theta, keys = dictionary_to_vector(parameters)
+    # parameters = vector_to_dictionary(theta, L)
+    # print('parameters:', parameters)
+    
 
-# # print(theta.shape, len(keys) )
+    Y, Z = deepnet.FFNN_paper_model(X, parameters)
 
-# phi, Y, Z = deepnet.FFNN_paper_model(X, parameters)
+    # # Compute O operator
+    # O_W1, O_b1 = gradient_descent.compute_O_operator(X, Z, parameters)
+    # print('O_W1:', O_W1)
+    # print('O_b1:', O_b1)
 
-# # generate train_set
-# train_set, train_set_phi = cost_function.markov_chain(X, phi, parameters, n_sample = 10)
+    # O_theta = O_gradients_to_vector(O_W1, O_b1)
+    # print(O_theta)
 
-# grads = gradient_descent.compute_gradients(train_set, parameters)
-
-# difference = gradients_check(parameters, grads, train_set, train_set_phi, epsilon = 1e-7)
+    # # generate train_set
+    # train_set, train_set_Y = cost_function.markov_chain(X, Y, parameters, n_sample = 1000)
+    # hamiltonian, train_set_h_local = cost_function.compute_hamiltonian(train_set, train_set_Y, parameters)
+    # f = gradient_descent.compute_f(train_set, train_set_h_local, parameters, hamiltonian)
+    # f_theta = f_to_vector(f)
+    # print('f:',f)
+    # print(f_theta)
+    
 

gradient_descent.py
@@ -162,14 +162,15 @@ def compute_f(train_set, train_set_h_local, parameters, hamiltonian):
             f_W1 -- Gradient of H with respect to W1
             f_b1 -- Gradient of H with respect to b1
     """
+    L = parameters['W1'].shape[1]
 
     n_sample = len(train_set)
 
-    quant_ave_OW1_EL = np.zeros((parameters['W1'].shape[0], L))
-    quant_ave_Ob1_EL = np.zeros((parameters['b1'].shape[0], 1))
+    quant_ave_OW1_EL = np.zeros(parameters['W1'].shape)
+    quant_ave_Ob1_EL = np.zeros(parameters['b1'].shape)
 
-    quant_ave_OW1 = np.zeros((parameters['W1'].shape[0], L))
-    quant_ave_Ob1 = np.zeros((parameters['b1'].shape[0], 1))
+    quant_ave_OW1 = np.zeros(parameters['W1'].shape)
+    quant_ave_Ob1 = np.zeros(parameters['b1'].shape)
     
 
     for i in range(n_sample):

stochastic_reconfiguration.py
@@ -5,51 +5,66 @@ import gradient_descent
 import grad_check
 import matplotlib.pyplot as plt
 
+
 ### Compute the S matrix
 
-def compute_S_matrix(train_set, parameters):
+def compute_S(train_set, parameters):
     """
     Compute the S matrix for Stochastic Reconfiguration
+    The algorithm is similar to compute f 
 
     Arguments:
-    train_set -- train_set -- Training set with each row representing one |sigma>
+    train_set -- Training set with each row representing one |sigma>
     parameters -- python dictionary containing your parameters: Wl, bl
 
     Returns:
-    S_matrix
+    S -- the S matrix for Stochastic Reconfiguration
     """
 
     n_sample = len(train_set)
 
-    O_thetas = {} # Python dictionary containing all O_theta's: O_thetas['x0'], O_thetas['x1'], ... 
+    O_thetas = {} # Python dictionary to store all O_theta for each spin_configuration
 
     for i in range(len(train_set)):
         spin_configuration = train_set[i].reshape((-1,1))
-        phi, Y, Z = deepnet.FFNN_paper_model(spin_configuration, parameters)
-        O_grads = gradient_descent.compute_O_operator(spin_configuration, parameters, Z )
-        O_thetas['x' + str(i)] = grad_check.O_gradients_to_vector(O_grads)
+        Y, Z = deepnet.FFNN_paper_model(spin_configuration, parameters)
+        O_W1, O_b1 = gradient_descent.compute_O_operator(spin_configuration, Z, parameters)
+        O_theta = grad_check.O_gradients_to_vector(O_W1, O_b1)
+        # print('O_theta shape:', O_theta.shape)
+        O_thetas['sigma'+str(i)] = O_theta
 
-        if i == 0:
-            O_thetas_average = np.copy(O_thetas['x0'])
-        else:
-            O_thetas_average = O_thetas_average + O_thetas['x' + str(i)]
-        """ The above O_thetas_average has not been divided by n_sample"""
+    K = O_thetas['sigma0'].shape[0]
+    # # Compute S_check elementwise
+    """ This is only for check. It should not be used in iteration as it's too slow. """
+
+    # S_check = np.zeros((K,K), dtype=complex)
+
+    # for k in range(K):
+    #     for k_prime in range(K):
+
+    #         first_term = 0
+    #         second_term = 0
+    #         third_term = 0
+
+    #         for i in range(n_sample):
+    #             first_term += np.conjugate(O_thetas['sigma'+str(i)].item(k)) * O_thetas['sigma'+str(i)].item(k_prime)
+    #             second_term += np.conjugate(O_thetas['sigma'+str(i)].item(k))
+    #             third_term += O_thetas['sigma'+str(i)].item(k_prime)
+
+    #         S_check.itemset((k,k_prime), first_term/n_sample - second_term / n_sample * third_term / n_sample)
+
     
-    O_thetas_average = O_thetas_average / n_sample
+    # Compute S using algorithm to speed up
+    quant_ave_O_star_O = np.zeros((K, K), dtype=complex)
+    quant_ave_O = np.zeros((K, 1), dtype=complex)
+
+    for i in range(n_sample):
+        quant_ave_O_star_O = quant_ave_O_star_O + np.dot(np.conjugate(O_thetas['sigma'+str(i)]), O_thetas['sigma'+str(i)].T)
+        quant_ave_O = quant_ave_O + O_thetas['sigma'+str(i)]
     
-    for i in range(len(train_set)):
-        O_diff = O_thetas['x'+str(i)] - O_thetas_average
-        
-        if i == 0:
-            S_matrix = np.copy( np.dot(O_diff, O_diff.T) )
-        else:
-            S_matrix = S_matrix + ( np.dot( O_diff, O_diff.T) )
-        """ The above S matrix has not been divided by n_sample"""
+    S = quant_ave_O_star_O / n_sample - np.dot(np.conjugate(quant_ave_O), quant_ave_O.T) / (n_sample**2)
 
-    S_matrix = np.real( S_matrix / n_sample )
-
-    assert(S_matrix.shape == (O_thetas_average.shape[0], O_thetas_average.shape[0]))
-    return S_matrix
+    return S
 
 
 
@@ -71,7 +86,7 @@ def regularize_S_matrix(S_matrix, epsilon):
 
 ### Update Parameters
 
-def sr_update_parameters(parameters, S_matrix_reg, grads, learning_rate):
+def sr_update_parameters(parameters, S_matrix_reg, f, learning_rate):
     """
     Update parameters using gradient descent
     
@@ -84,68 +99,77 @@ def sr_update_parameters(parameters, S_matrix_reg, grads, learning_rate):
                   parameters["W" + str(l)] = ... 
                   parameters["b" + str(l)] = ...
     """
+    L = parameters['W1'].shape[1]
    
-    new_theta = grad_check.dictionary_to_vector(parameters)[0] - learning_rate * np.dot(np.linalg.pinv(S_matrix_reg) , grad_check.gradients_to_vector(grads) )
+    new_theta = grad_check.dictionary_to_vector(parameters) - learning_rate * np.dot(np.linalg.pinv(S_matrix_reg) , grad_check.f_to_vector(f) )
     
-    parameters = grad_check.vector_to_dictionary(new_theta)
+    parameters = grad_check.vector_to_dictionary(new_theta, L)
     
     return parameters
 
 
 
 ### Test
+if __name__ == '__main__':
 
-L = 6
-# input
-X = np.array([-1,  1,  -1, 1, -1, 1]).reshape((L,1))
+    L = 6
+    # input
+    X = np.array([-1,  1,  -1, 1, -1, 1]).reshape((L,1))
 
-# Initialize parameters, then retrieve W1, b1, W2, b2. 
+    # Initialize parameters, then retrieve W1, b1, W2, b2. 
+    parameters = deepnet.initialize_parameters(L, n_h = 2*L, seed=1234, sigma=0.01)
+    
+    # Y, Z = deepnet.FFNN_paper_model(X, parameters)
 
-parameters = deepnet.initialize_parameters(L, n_h = 2*L, seed=1234, sigma=0.01)
+    # train_set, train_set_Y = cost_function.markov_chain(X, Y, parameters, n_sample = 5)
 
-costs = []
-num_iterations = 300
-
-
-for iter in range(0, num_iterations):
-    # Feedforward
-
-    phi, Y, Z = deepnet.FFNN_paper_model(X, parameters)
-
-    # Generate train_set
-
-    train_set, train_set_phi = cost_function.markov_chain(X, phi, parameters, n_sample = 1000)
-
-    # Forward propagation: LINEAR -> RELU -> LINEAR -> SIGMOID. Inputs: "X, W1, b1". Output: "A1, cache1, A2, cache2".
-    """ Finished above and in the process of generating the train_set """
-        
-    # Compute cost
-    hamiltonian = cost_function.compute_hamiltonian(train_set, train_set_phi, parameters)
-
-
-    # Backward propagation. Inputs: "dA2, cache2, cache1". Outputs: "dA1, dW2, db2; also dA0 (not used), dW1, db1".
-    grads = gradient_descent.compute_gradients(train_set, parameters)
-    S_matrix = compute_S_matrix(train_set, parameters)
-    S_matrix_reg = regularize_S_matrix(S_matrix, epsilon = 0.00001)
-        
-    # Update parameters.
-    parameters = sr_update_parameters(parameters, S_matrix_reg, grads, 0.001)
-                                           
-    # Retrieve W1, b1, W2, b2 from parameters
-    W1 = parameters["W1"]
-    b1 = parameters["b1"]
-    # W2 = parameters["W2"]
-    # b2 = parameters["b2"]
-
-    # Print the cost
-    print("Cost after iteration {}: {}".format(iter, np.squeeze(hamiltonian)))
-    costs.append(hamiltonian)
+    # S = compute_S(train_set, parameters)
+    # print("S_check:", S_check)
     
 
-plt.plot(np.squeeze(costs))
-plt.ylabel('cost')
-plt.xlabel('iterations (per tens)')
-plt.title("Learning rate =" + str(0.005))
-plt.show()
+    costs = []
+    num_iterations = 600
+
+
+    for iter in range(0, num_iterations):
+        # Feedforward
+
+        Y, Z = deepnet.FFNN_paper_model(X, parameters)
+
+        # Generate train_set
+
+        train_set, train_set_Y = cost_function.markov_chain(X, Y, parameters, n_sample = 1000)
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> SIGMOID. Inputs: "X, W1, b1". Output: "A1, cache1, A2, cache2".
+        """ Finished above and in the process of generating the train_set """
+            
+        # Compute cost
+        hamiltonian, train_set_h_local = cost_function.compute_hamiltonian(train_set, train_set_Y, parameters)
+
+        # Compute f
+        f = gradient_descent.compute_f(train_set, train_set_h_local, parameters, hamiltonian)
+        
+        # Compute S
+        S = compute_S(train_set, parameters)
+            
+        # Update parameters.
+        parameters = sr_update_parameters(parameters, S, f, 0.005)
+                                            
+        # Retrieve W1, b1, W2, b2 from parameters
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        # W2 = parameters["W2"]
+        # b2 = parameters["b2"]
+
+        # Print the cost
+        print("Cost after iteration {}: {}".format(iter, np.squeeze(hamiltonian)))
+        costs.append(hamiltonian)
+        
+
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(0.005))
+    plt.show()
 
 